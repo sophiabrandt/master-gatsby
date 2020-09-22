e8f19b6dd5c5c4051e429f122ff3f9f9d5aad265
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-09-22 21:42:55 +0200

    📦 NEW: Add initial pages

---
+++ gatsby/src/pages/pizzas.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PizzasPage() {
+  return (
+    <div>
+      <p>This is the pizzas page!</p>
+    </div>
+  )
+}
---
+++ gatsby/src/pages/slicemasters.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SliceMastersPage() {
+  return (
+    <div>
+      <p>This is the slice-masters page!</p>
+    </div>
+  )
+}
