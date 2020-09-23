e4939502261244700ae1182f049d9df41b41191c
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-09-23 17:18:44 +0200

    📦 NEW: Add Typopgraphy

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types'
 import Nav from './Nav'
 import Footer from './Footer'
 import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
 import 'normalize.css'
 
 export default function Layout({ children }) {
   return (
     <div>
       <GlobalStyles />
+      <Typography />
       <Nav />
       {children}
       <Footer />
