a75c260d1f46b0df15ff2838775ef5d5eaac9865
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-09-23 17:13:13 +0200

    📦 NEW: Add global styles

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -2,10 +2,13 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Nav from './Nav'
 import Footer from './Footer'
+import GlobalStyles from '../styles/GlobalStyles';
+import 'normalize.css'
 
 export default function Layout({ children }) {
   return (
     <div>
+      <GlobalStyles />
       <Nav />
       {children}
       <Footer />
