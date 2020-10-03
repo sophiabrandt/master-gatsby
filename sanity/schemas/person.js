da9798a0f018c614d9ecb1ea2542a99fa2237ead
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-10-03 09:54:56 +0200

    📦 NEW: Add person schema

---
+++ sanity/schemas/person.js
@@ -0,0 +1,39 @@
+import { BsPersonFill as icon } from 'react-icons/bs'
+
+export default {
+  // schema name
+  name: 'person',
+  title: 'Slice Masters',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+      description: 'Tell us a bit about them!'
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+}
