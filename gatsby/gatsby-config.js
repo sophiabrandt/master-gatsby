c56012f7e904a82d35ba6ad6f266d45de60f5359
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-10-04 12:29:14 +0200

    📦 NEW: Add sanity config

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,1 +1,25 @@
-// This file is empty, but some people were reporting that it would not start unless they had an empty file. So here it is! You can delete the comment. Or replace it with your favourite shania twain lyrics.
+import dotenv from 'dotenv'
+
+dotenv.config({ path: './env' })
+
+console.log(process.env.SANITY_TOKEN)
+
+export default {
+  siteMetadata: {
+    title: 'Slicks Slices',
+    siteUrl: 'https://wwww.gatsby.pizza',
+    description: 'The best pizza in town!',
+  },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'vtpxf73d',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
+}
