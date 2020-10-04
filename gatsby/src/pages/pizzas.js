77e5e7f9c49e19d43531ca97f4a17aacca780275
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-10-04 13:13:41 +0200

    📦 NEW: Add Pizzas from back-end

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+import PizzaList from '../components/PizzaList'
 
-export default function PizzasPage() {
-  return (
-    <>
-      <p>This is the pizzas page!</p>
-    </>
-  )
+export default function PizzasPage({
+  data: {
+    pizzas: { nodes: pizzas },
+  },
+}) {
+  return <PizzaList pizzas={pizzas} />
 }
+
+export const query = graphql`
+  query {
+    pizzas: allSanityPizza {
+      nodes {
+        id
+        name
+        slug {
+          current
+        }
+        toppings {
+          name
+          id
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
