95aa41cde1ddfe80cfebea31ca054be277facbe0
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-10-12 15:16:06 +0200

    📦 NEW: Source beers from external API & style

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -1,4 +1,5 @@
 import path from 'path'
+import fetch from 'isomorphic-fetch'
 
 async function turnPizzasIntoPages({ graphql, actions }) {
   const pizzaTemplate = path.resolve('./src/templates/Pizza.js')
@@ -50,6 +51,33 @@ async function turnToppingsIntoPages({ graphql, actions }) {
       context: { topping: topping.name, toppingRegex: `/${topping.name}/i` },
     })
   })
+}
+
+async function fetchBeersAndTurnIntoNodes({actions, createNodeId, createContentDigest}) {
+  const res = await fetch('https://sampleapis.com/beers/api/ale')
+  const beers = await res.json()
+  for (const beer of beers) {
+    const nodeMeta = {
+      id: createNodeId(`beers-${beer.name}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: 'Beer',
+        mediaType: 'application/json',
+        contentDigest: createContentDigest(beer),
+      }
+    }
+    actions.createNode({
+      ...beer,
+      ...nodeMeta
+    })
+  }
+}
+
+export async function sourceNodes(params) {
+  await Promise.all([
+    fetchBeersAndTurnIntoNodes(params)
+  ])
 }
 
 export async function createPages(params) {
--- gatsby/src/pages/beers.js
+++ gatsby/src/pages/beers.js
@@ -1,9 +1,73 @@
+import styled from 'styled-components';
 import React from 'react'
+import { graphql } from 'gatsby'
 
-export default function BeersPage() {
+const BeerGridStyle = styled.div`
+  display: grid;
+  gap: 2rem;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+`
+
+const SingleBeerStyle = styled.div`
+  border: 1px solid var(--grey);
+  padding: 2rem;
+  text-align: center;
+  img {
+    width: 100%;
+    height: 200px;
+    object-fit: contain;
+    display: grid;
+    line-items: center;
+    font-size: 10px;
+  }
+`
+
+export default function BeersPage({
+  data: {
+    beers: { nodes: beers },
+  },
+}) {
   return (
     <>
-      <p>This is the beers page!</p>
+      <h2 className="center">
+        We have {beers.length} beers available. Dine in only!
+      </h2>
+    <BeerGridStyle>
+      {beers.map((beer) => {
+        const rating = Math.round(beer.rating.average)
+        return (
+          <SingleBeerStyle key={beer.id}>
+            <img src={beer.image} alt={beer.name} />
+            <h3>{beer.name}</h3>
+            {beer.price}
+            <p title={`${rating} out of 5 stars`}>
+              {`⭐`.repeat(rating)}
+              <span style={{ filter: `grayScale(100%)` }}>
+                {`⭐`.repeat(5 - rating)}
+              </span>
+              <span>({beer.rating.reviews})</span>
+            </p>
+          </SingleBeerStyle>
+        )
+      })}
+    </BeerGridStyle>
     </>
   )
 }
+
+export const query = graphql`
+  query {
+    beers: allBeer {
+      nodes {
+        id
+        name
+        price
+        image
+        rating {
+          average
+          reviews
+        }
+      }
+    }
+  }
+`
