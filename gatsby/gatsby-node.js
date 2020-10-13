20d6a5141de44ce349a09fc557d71dfd606fea00
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-10-13 18:31:25 +0200

    📦 NEW: Create pages for persons (slicemasters)

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -53,7 +53,11 @@ async function turnToppingsIntoPages({ graphql, actions }) {
   })
 }
 
-async function fetchBeersAndTurnIntoNodes({actions, createNodeId, createContentDigest}) {
+async function fetchBeersAndTurnIntoNodes({
+  actions,
+  createNodeId,
+  createContentDigest,
+}) {
   const res = await fetch('https://sampleapis.com/beers/api/ale')
   const beers = await res.json()
   for (const beer of beers) {
@@ -65,21 +69,56 @@ async function fetchBeersAndTurnIntoNodes({actions, createNodeId, createContentD
         type: 'Beer',
         mediaType: 'application/json',
         contentDigest: createContentDigest(beer),
-      }
+      },
     }
     actions.createNode({
       ...beer,
-      ...nodeMeta
+      ...nodeMeta,
     })
   }
 }
 
+async function turnSlicemastersIntoPages({ graphql, actions }) {
+  const {
+    data: { slicemasters },
+  } = await graphql(`
+    query {
+      slicemasters: allSanityPerson {
+        totalCount
+        nodes {
+          name
+          id
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `)
+  const pageSize = Number(process.env.GATSBY_PAGE_SIZE)
+  const pageCount = Math.ceil(slicemasters.totalCount / pageSize)
+  Array.from({ length: pageCount }).forEach((_, i) => {
+    console.log(`Creating page ${i}`)
+    actions.createPage({
+      path: `/slicemasters/${i + 1}`,
+      component: path.resolve('./src/pages/slicemasters.js'),
+      context: {
+        skip: i * pageSize,
+        currentPage: i + 1,
+        pageSize,
+      },
+    })
+  })
+}
+
 export async function sourceNodes(params) {
-  await Promise.all([
-    fetchBeersAndTurnIntoNodes(params)
-  ])
+  await Promise.all([fetchBeersAndTurnIntoNodes(params)])
 }
 
 export async function createPages(params) {
-  await Promise.all([turnPizzasIntoPages(params), turnToppingsIntoPages(params)])
+  await Promise.all([
+    turnPizzasIntoPages(params),
+    turnToppingsIntoPages(params),
+    turnSlicemastersIntoPages(params),
+  ])
 }
