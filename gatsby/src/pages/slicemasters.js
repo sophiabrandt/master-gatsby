1b43f08c4c05654b1961786827df4ddddbd656fe
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-10-13 17:52:04 +0200

    📦 NEW: Add slicemasters page

--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -1,9 +1,83 @@
 import React from 'react'
+import { Link, graphql } from 'gatsby'
+import Img from 'gatsby-image'
+import styled from 'styled-components'
 
-export default function SliceMastersPage() {
+const SlicemasterGrid = styled.div`
+  display: grid;
+  grid-gap: 2rem;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+`
+
+const SlicemasterStyles = styled.div`
+  a {
+    text-decoration: none;
+  }
+  .gatsby-image-wrapper {
+    height: 400px;
+  }
+  h2 {
+    transfrom: rotate(-2deg);
+    text-align: center;
+    font-size: 4rem;
+    margin-bottom: -2rem;
+    position: relative;
+    z-index: 2;
+  }
+  .description {
+    background: var(--yellow);
+    padding: 1rem;
+    margin-top: -6rem;
+    z-index: 2;
+    position: relative;
+    transform: rotate(1deg);
+    text-align: center;
+  }
+`
+
+export default function SliceMastersPage({
+  data: {
+    slicemasters: { nodes: slicemasters },
+  },
+}) {
   return (
     <>
-      <p>This is the slice-masters page!</p>
+      <SlicemasterGrid>
+        {slicemasters.map((person) => (
+          <SlicemasterStyles key={person.id}>
+            <Link to={`/slicemasters/${person.slug.current}`}>
+              <h2>
+                <span className="mark">{person.name}</span>
+              </h2>
+            </Link>
+            <Img fluid={person.image.asset.fluid} />
+            <p className="description">{person.description}</p>
+          </SlicemasterStyles>
+        ))}
+      </SlicemasterGrid>
     </>
   )
 }
+
+export const query = graphql`
+  query {
+    slicemasters: allSanityPerson {
+      totalCount
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        description
+        image {
+          asset {
+            fluid(maxWidth: 410) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
